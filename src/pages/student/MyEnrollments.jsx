5b81dbc516a01e41ee3e3d82256c0affe17b5cf1
ocjdagreat1 <oparachibuikejustine@gmail.com>
2026-02-02 16:31:54 -0800

    new changes

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -27,7 +27,8 @@ const MyEnrollments = () => {
     <>
     <div className='md:px-36 px-8 pt-10'>
      <h1 className='text-2xl font-semibold'>My Enrollments</h1> 
-     <table className='md:table-auto table-fixed w-full overflow-hidden border mt-10'>
+      <div className="overflow-x-auto">
+    <table className='md:table-auto table-fixed w-full border mt-10'>
       <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden'>
         <tr>
           <th className='px-4 py-3 font-semibold truncate'>Course</th>
@@ -63,6 +64,7 @@ const MyEnrollments = () => {
         ))}
       </tbody>
      </table>
+    </div>
     </div>
 
     <Footer/>
